58dd5613d6ad1c091937efa10b8890750f4f9cc8
RajInGit1 <156179397+RajInGit1@users.noreply.github.com>
2026-07-29 21:54:57 +0530

    Add files via upload

---
+++ PyDict.py
@@ -0,0 +1,14 @@
+# Create a dictionary
+student = {
+    "name": "John",
+    "age": 20,
+    "course": "Python"
+}
+
+print(student)
+
+student = {"name": "John", "age": 20}
+
+student["city"] = "New York"
+
+print(student)
